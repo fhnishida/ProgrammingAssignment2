c472e950de48e64fb3f274b42cf14788a6f2792b
fhnishida <fhnishida@gmail.com>
2020-08-03 11:42:16 -0300

    Assignment 2 Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special "matrix" object that can
+## cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated
+## (and the matrix has not changed), then cacheSolve should retrieve the inverse
+## from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
+
+
+## Suppose the following inversible matrix A:
+A <- matrix(c(1, 0, 0, 0, 1, 1, 0, 1, -1), 3, 3)
+A
+
+## With the following inverse matrix of A
+A_inverse <- solve(A)
+A_inverse
+
+## Solving with the created function
+A1 <- makeCacheMatrix(A)
+cacheSolve(A1)
+
+## We can conclude that the function is correct, since
+all(A_inverse == cacheSolve(A1))
